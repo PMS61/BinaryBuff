b5b8983fe9f528794de668589a3ce24e07097130
Prathamesh Sankhe <pmsankhe_b23@it.vjti.ac.in>
2025-03-21 11:12:35 +0530

    Initialised Repo

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.binarybuff.app',
+  appName: 'binarybuff',
+  webDir: 'public'
+};
+
+export default config;
